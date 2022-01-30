36b431b8e53e337e60f61b5f10c33e94cd4510bd
Glatorian13 <glatorian13@gmail.com>
2022-01-30 18:38:29 -0500

    restyle, switches, use this version

--- Date.java
+++ Date.java
@@ -2,187 +2,189 @@
 import java.util.Scanner;
 
 public class Date {
-	private String month;
-	private int day;
-	private int year; // a four digit number.
+  private String month;
+  private int day;
+  private int year; // a four digit number.
 
-	public Date() {
-		month = "January";
-		day = 1;
-		year = 1000;
-	}
+  public Date() {
+    month = "January";
+    day = 1;
+    year = 1000;
+  }
 
-	public Date(int monthInt, int day, int year) {
-		setDate(monthInt, day, year);
-	}
+  public Date(int monthInt, int day, int year) {
+    setDate(monthInt, day, year);
+  }
 
-	public Date(String monthString, int day, int year) {
-		setDate(monthString, day, year);
-	}
+  public Date(String monthString, int day, int year) {
+    setDate(monthString, day, year);
+  }
 
-	public Date(int year) {
-		setDate(1, 1, year);
-	}
+  public Date(int year) {
+    setDate(1, 1, year);
+  }
 
-	public Date(Date aDate) {
-		if (aDate == null)// Not a real date.
-		{
-			System.out.println("Fatal Error.");
-			System.exit(0);
-		}
+  public Date(Date aDate) {
+    if (aDate == null)// Not a real date.
+    {
+      System.out.println("Fatal Error.");
+      System.exit(0);
+    }
 
-		month = aDate.month;
-		day = aDate.day;
-		year = aDate.year;
-	}
+    month = aDate.month;
+    day = aDate.day;
+    year = aDate.year;
+  }
 
-	public void setDate(int monthInt, int day, int year) {
-		if (dateOK(monthInt, day, year)) {
-			this.month = monthString(monthInt);
-			this.day = day;
-			this.year = year;
-		} else {
-			System.out.println("Fatal Error");
-			System.exit(0);
-		}
-	}
+  public void setDate(int monthInt, int day, int year) {
+    if (dateOK(monthInt, day, year)) {
+      this.month = monthString(monthInt);
+      this.day = day;
+      this.year = year;
+    } else {
+      System.out.println("Fatal Error");
+      System.exit(0);
+    }
+  }
 
-	public void setDate(String monthString, int day, int year) {
-		if (dateOK(monthString, day, year)) {
-			this.month = monthString;
-			this.day = day;
-			this.year = year;
-		} else {
-			System.out.println("Fatal Error");
-			System.exit(0);
-		}
-	}
+  public void setDate(String monthString, int day, int year) {
+    if (dateOK(monthString, day, year)) {
+      this.month = monthString;
+      this.day = day;
+      this.year = year;
+    } else {
+      System.out.println("Fatal Error");
+      System.exit(0);
+    }
+  }
 
-	public void setDate(int year) {
-		setDate(1, 1, year);
-	}
+  public void setDate(int year) {
+    setDate(1, 1, year);
+  }
 
-	public void setYear(int year) {
-		if ((year < 1000) || (year > 9999)) {
-			System.out.println("Fatal Error");
-			System.exit(0);
-		} else
-			this.year = year;
-	}
+  public void setYear(int year) {
+    if ((year < 1000) || (year > 9999)) {
+      System.out.println("Fatal Error");
+      System.exit(0);
+    } else
+      this.year = year;
+  }
 
-	public void setMonth(int monthNumber) {
-		if ((monthNumber <= 0) || (monthNumber > 12)) {
-			System.out.println("Fatal Error");
-			System.exit(0);
-		} else
-			month = monthString(monthNumber);
-	}
+  public void setMonth(int monthNumber) {
+    if ((monthNumber <= 0) || (monthNumber > 12)) {
+      System.out.println("Fatal Error");
+      System.exit(0);
+    } else
+      month = monthString(monthNumber);
+  }
 
-	public void setDay(int day) {
-		if ((day <= 0) || (day > 31)) {
-			System.out.println("Fatal Error");
-			System.exit(0);
-		} else
-			this.day = day;
-	}
+  public void setDay(int day) {
+    if ((day <= 0) || (day > 31)) {
+      System.out.println("Fatal Error");
+      System.exit(0);
+    } else
+      this.day = day;
+  }
 
-	public int getMonth() {
-		return switch(month.toLowerCase()) {
-		case "january" -> 1;
-		case "february" -> 2;
-		case "march" -> 3;
-		case "april" -> 4;
-		case "may" -> 5;
-		case "june" -> 6;
-		case "july" -> 7;
-		case "august" -> 8;
-		case "september" -> 9;
-		case "october" -> 10;
-		case "november" -> 11;
-		case "december" -> 12;
-		default -> {
-				System.out.println("Fatal Error");
-				System.exit(0);
-				yield 0; // Needed to keep the compiler happy
-				}
-			};
-		}
+  public int getMonth() {
+    return switch (month.toLowerCase()) {
+      case "january" -> 1;
+      case "february" -> 2;
+      case "march" -> 3;
+      case "april" -> 4;
+      case "may" -> 5;
+      case "june" -> 6;
+      case "july" -> 7;
+      case "august" -> 8;
+      case "september" -> 9;
+      case "october" -> 10;
+      case "november" -> 11;
+      case "december" -> 12;
+      default -> {
+        System.out.println("Fatal Error");
+        System.exit(0);
+        yield 0; // Needed to keep the compiler happy
+      }
+    };
+  }
 
-	public int getDay() {
-		return day;
-	}
+  public int getDay() {
+    return day;
+  }
 
-	public int getYear() {
-		return year;
-	}
+  public int getYear() {
+    return year;
+  }
 
-	public String toString() {
-		return (month + " " + day + ", " + year);
-	}
+  public String toString() {
+    return (month + " " + day + ", " + year);
+  }
 
-	public boolean equals(Date otherDate) {
-		return ((month.equals(otherDate.month)) && (day == otherDate.day) && (year == otherDate.year));
-	}
+  public boolean equals(Date otherDate) {
+    return ((month.equals(otherDate.month)) && (day == otherDate.day) && (year == otherDate.year));
+  }
 
-	public boolean precedes(Date otherDate) {
-		return ((year < otherDate.year) || (year == otherDate.year && getMonth() < otherDate.getMonth())
-				|| (year == otherDate.year && month.equals(otherDate.month) && day < otherDate.day));
-	}
+  public boolean precedes(Date otherDate) {
+    return ((year < otherDate.year) || (year == otherDate.year && getMonth() < otherDate.getMonth())
+        || (year == otherDate.year && month.equals(otherDate.month) && day < otherDate.day));
+  }
 
-	public void readInput() {
-		var tryAgain = true;
-		var keyboard = new Scanner(System.in);
-		while (tryAgain) {
-			System.out.println("Enter month, day, and year.");
-			System.out.println("Do not use a comma.");
-			String monthInput = keyboard.next();
-			var dayInput = keyboard.nextInt();
-			var yearInput = keyboard.nextInt();
-			if (dateOK(monthInput, dayInput, yearInput)) {
-				setDate(monthInput, dayInput, yearInput);
-				tryAgain = false;
-			} else
-				System.out.println("Illegal date. Reenter input.");
-		}
-	}
+  public void readInput() {
+    var tryAgain = true;
+    var keyboard = new Scanner(System.in);
+    while (tryAgain) {
+      System.out.println("Enter month, day, and year.");
+      System.out.println("Do not use a comma.");
+      String monthInput = keyboard.next();
+      var dayInput = keyboard.nextInt();
+      var yearInput = keyboard.nextInt();
+      if (dateOK(monthInput, dayInput, yearInput)) {
+        setDate(monthInput, dayInput, yearInput);
+        tryAgain = false;
+      } else
+        System.out.println("Illegal date. Reenter input.");
+    }
+    // added to close resource leak
+    keyboard.close();
+  }
 
-	private boolean dateOK(int monthInt, int dayInt, int yearInt) {
-		return ((monthInt >= 1) && (monthInt <= 12) && (dayInt >= 1) && (dayInt <= 31) && (yearInt >= 1000)
-				&& (yearInt <= 9999));
-	}
+  private boolean dateOK(int monthInt, int dayInt, int yearInt) {
+    return ((monthInt >= 1) && (monthInt <= 12) && (dayInt >= 1) && (dayInt <= 31)
+        && (yearInt >= 1000) && (yearInt <= 9999));
+  }
 
-	private boolean dateOK(String monthString, int dayInt, int yearInt) {
-		return (monthOK(monthString) && (dayInt >= 1) && (dayInt <= 31) && (yearInt >= 1000) && (yearInt <= 9999));
-	}
-	
-	private boolean monthOK(String month) {
-		return switch(month) {
-		case "January", "February", "March", "April", "May", "June", "July",
-		"August", "September", "October", "November", "December" -> true;
-		default -> false;
-		};
-	}
+  private boolean dateOK(String monthString, int dayInt, int yearInt) {
+    return (monthOK(monthString) && (dayInt >= 1) && (dayInt <= 31) && (yearInt >= 1000)
+        && (yearInt <= 9999));
+  }
 
-	private String monthString(int monthNumber) {
-	return switch(monthNumber) {
-	case 1 -> "January";
-	case 2 -> "February";
-	case 3 -> "March";
-	case 4 -> "April";
-	case 5 -> "May";
-	case 6 -> "June";
-	case 7 -> "July";
-	case 8 -> "August";
-	case 9 -> "September";
-	case 10 -> "October";
-	case 11 -> "November";
-	case 12 -> "December";
-	default -> {
-			System.out.println("Fatal Error");
-			System.exit(0);
-			yield "Error"; // to keep the compiler happy
-			}
-		};
-	}
- 
+  private boolean monthOK(String month) {
+    return switch (month) {
+      case "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" -> true;
+      default -> false;
+    };
+  }
+
+  private String monthString(int monthNumber) {
+    return switch (monthNumber) {
+      case 1 -> "January";
+      case 2 -> "February";
+      case 3 -> "March";
+      case 4 -> "April";
+      case 5 -> "May";
+      case 6 -> "June";
+      case 7 -> "July";
+      case 8 -> "August";
+      case 9 -> "September";
+      case 10 -> "October";
+      case 11 -> "November";
+      case 12 -> "December";
+      default -> {
+        System.out.println("Fatal Error");
+        System.exit(0);
+        yield "Error"; // to keep the compiler happy
+      }
+    };
+  }
+
 }
--- DateTest.java
+++ DateTest.java
@@ -9,178 +9,178 @@ import org.junit.Test;
 import org.junit.*;
 
 public class DateTest {
-	
-	//stuff testing user input
-		// https://stackoverflow.com/questions/1647907/junit-how-to-simulate-system-in-testing
-		private final InputStream sysIn = System.in;
-		private final PrintStream sysOut = System.out;
-		
-		private ByteArrayInputStream testIn;
-		private ByteArrayOutputStream testOut;
-		@Before
-		public void setUpOut() {
-			testOut = new ByteArrayOutputStream();
-			System.setOut(new PrintStream(testOut));
-		}
-		
-		private void provideIn(String data) {
-			testIn = new ByteArrayInputStream(data.getBytes());
-			System.setIn(testIn);
-		}
-		
-		/*
-		 * private String getOut() { return testOut.toString(); }
-		 */
-		@After
-		public void restoreOriginalInOut() {
-			System.setIn(sysIn);
-			System.setOut(sysOut);
-		}
 
-	/*
-	 * Cannot test methods that use System.exit because Eclipse is using JUnit 4.11.
-	 * If on version 4.9 or newer, testing System.exit is simple Java 17 deprecated
-	 * the security manager that you can use to test
-	 */
+  // stuff testing user input
+  // https://stackoverflow.com/questions/1647907/junit-how-to-simulate-system-in-testing
+  private final InputStream sysIn = System.in;
+  private final PrintStream sysOut = System.out;
 
-	@Test
-	public void testDate() {
-		Date testDate = new Date();
-		assertEquals(1, testDate.getMonth());
-		assertEquals(1, testDate.getDay());
-		assertEquals(1000, testDate.getYear());
-	}
+  private ByteArrayInputStream testIn;
+  private ByteArrayOutputStream testOut;
 
-	@Test
-	public void testSetDateIntIntInt() {
-		Date mySetDateIntIntInt = new Date();
-		mySetDateIntIntInt.setDate(2, 3, 1994);
-		assertEquals(2, mySetDateIntIntInt.getMonth());
-		assertEquals(3, mySetDateIntIntInt.getDay());
-		assertEquals(1994, mySetDateIntIntInt.getYear());
-	}
+  @Before
+  public void setUpOut() {
+    testOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(testOut));
+  }
 
-	@Test
-	public void testSetDateStringIntInt() {
-		Date mySetDateStringIntInt = new Date();
-		mySetDateStringIntInt.setDate("July", 8, 2001);
-		// .getMonth() always returns number
-		assertEquals(7, mySetDateStringIntInt.getMonth());
-		assertEquals(8, mySetDateStringIntInt.getDay());
-		assertEquals(2001, mySetDateStringIntInt.getYear());
-	}
+  private void provideIn(String data) {
+    testIn = new ByteArrayInputStream(data.getBytes());
+    System.setIn(testIn);
+  }
 
-	@Test
-	public void testSetDateInt() {
-		Date mySetDateYearInt = new Date();
-		mySetDateYearInt.setDate(1990);
-		assertEquals(1990, mySetDateYearInt.getYear());
-	}
+  /*
+   * private String getOut() { return testOut.toString(); }
+   */
+  @After
+  public void restoreOriginalInOut() {
+    System.setIn(sysIn);
+    System.setOut(sysOut);
+  }
 
-	@Test
-	public void testSetYear() {
-		Date mySetYearInt = new Date();
-		for (int i = 1000; i < 9999; i++) {
-			mySetYearInt.setYear(i);
-			assertEquals(i, mySetYearInt.getYear());
-		}
-	}
+  /*
+   * Cannot test methods that use System.exit because Eclipse is using JUnit 4.11. If on version 4.9
+   * or newer, testing System.exit is simple Java 17 deprecated the security manager that you can
+   * use to test
+   */
 
-	@Test
-	public void testSetMonth() {
-		Date mySetMonthInt = new Date();
-		for (int i = 1; i < 13; i++) {
-			mySetMonthInt.setMonth(i);
-			assertEquals(i, mySetMonthInt.getMonth());
-		}
-		/*
-		 * mySetMonthInt.setMonth(0); assertEquals("Fatal Error",
-		 * mySetMonthInt.getMonth()); mySetMonthInt.setMonth(14);
-		 * assertEquals("Fatal Error", mySetMonthInt.getMonth());
-		 */
-	}
+  @Test
+  public void testDate() {
+    Date testDate = new Date();
+    assertEquals(1, testDate.getMonth());
+    assertEquals(1, testDate.getDay());
+    assertEquals(1000, testDate.getYear());
+  }
 
-	@Test
-	public void testSetDay() {
-		Date mySetDayInt = new Date();
+  @Test
+  public void testSetDateIntIntInt() {
+    Date mySetDateIntIntInt = new Date();
+    mySetDateIntIntInt.setDate(2, 3, 1994);
+    assertEquals(2, mySetDateIntIntInt.getMonth());
+    assertEquals(3, mySetDateIntIntInt.getDay());
+    assertEquals(1994, mySetDateIntIntInt.getYear());
+  }
 
-		for (int i = 1; i < 32; i++) {
-			mySetDayInt.setDay(i);
-			assertEquals(i, mySetDayInt.getDay());
-		}
-		/*
-		 * mySetDayInt.setDay(0); assertEquals("Fatal Error", mySetDayInt.getDay());
-		 * mySetDayInt.setDay(44); assertEquals("Fatal Error", mySetDayInt.getDay());
-		 */
-	}
+  @Test
+  public void testSetDateStringIntInt() {
+    Date mySetDateStringIntInt = new Date();
+    mySetDateStringIntInt.setDate("July", 8, 2001);
+    // .getMonth() always returns number
+    assertEquals(7, mySetDateStringIntInt.getMonth());
+    assertEquals(8, mySetDateStringIntInt.getDay());
+    assertEquals(2001, mySetDateStringIntInt.getYear());
+  }
 
-	@Test
-	public void testGetMonth() {
-		Date myGetMonthInt = new Date();
+  @Test
+  public void testSetDateInt() {
+    Date mySetDateYearInt = new Date();
+    mySetDateYearInt.setDate(1990);
+    assertEquals(1990, mySetDateYearInt.getYear());
+  }
 
-		for (int i = 1; i < 13; i++) {
-			myGetMonthInt.setMonth(i);
-			assertEquals(i, myGetMonthInt.getMonth());
-		}
-		/*
-		 * myGetMonthInt.setMonth(13); assertEquals(0, myGetMonthInt.getMonth());
-		 */
-	}
+  @Test
+  public void testSetYear() {
+    Date mySetYearInt = new Date();
+    for (int i = 1000; i < 9999; i++) {
+      mySetYearInt.setYear(i);
+      assertEquals(i, mySetYearInt.getYear());
+    }
+  }
 
-	@Test
-	public void testGetDay() {
-		Date myGetDay = new Date();
-		int i = 1;
-		while (i < 32) {
-			myGetDay.setDay(i);
-			assertEquals(i, myGetDay.getDay());
-			i++;
-		}
-	}
+  @Test
+  public void testSetMonth() {
+    Date mySetMonthInt = new Date();
+    for (int i = 1; i < 13; i++) {
+      mySetMonthInt.setMonth(i);
+      assertEquals(i, mySetMonthInt.getMonth());
+    }
+    /*
+     * mySetMonthInt.setMonth(0); assertEquals("Fatal Error", mySetMonthInt.getMonth());
+     * mySetMonthInt.setMonth(14); assertEquals("Fatal Error", mySetMonthInt.getMonth());
+     */
+  }
 
-	@Test
-	public void testGetYear() {
-		Date myGetYear = new Date();
-		assertEquals(1000, myGetYear.getYear());
-	}
+  @Test
+  public void testSetDay() {
+    Date mySetDayInt = new Date();
 
-	@Test
-	public void testToString() {
-		Date myToString = new Date();
-		assertEquals("January 1, 1000", myToString.toString());
-	}
+    for (int i = 1; i < 32; i++) {
+      mySetDayInt.setDay(i);
+      assertEquals(i, mySetDayInt.getDay());
+    }
+    /*
+     * mySetDayInt.setDay(0); assertEquals("Fatal Error", mySetDayInt.getDay());
+     * mySetDayInt.setDay(44); assertEquals("Fatal Error", mySetDayInt.getDay());
+     */
+  }
 
-	@Test
-	public void testEqualsDate() {
-		Date testDate1 = new Date();
-		Date testDate2 = new Date();
-		assertTrue(testDate1.equals(testDate2));
-	}
+  @Test
+  public void testGetMonth() {
+    Date myGetMonthInt = new Date();
 
-	@Test
-	public void testPrecedes() {
-		Date testDate1 = new Date();
-		Date testDate2 = new Date(2, 1, 1000);
-		Date testDate3 = new Date(1, 2, 1000);
-		Date testDate4 = new Date(1, 1, 2000);
-		assertTrue(testDate1.precedes(testDate2));
-		assertTrue(testDate1.precedes(testDate3));
-		assertTrue(testDate1.precedes(testDate4));
+    for (int i = 1; i < 13; i++) {
+      myGetMonthInt.setMonth(i);
+      assertEquals(i, myGetMonthInt.getMonth());
+    }
+    /*
+     * myGetMonthInt.setMonth(13); assertEquals(0, myGetMonthInt.getMonth());
+     */
+  }
 
-	}
+  @Test
+  public void testGetDay() {
+    Date myGetDay = new Date();
+    int i = 1;
+    while (i < 32) {
+      myGetDay.setDay(i);
+      assertEquals(i, myGetDay.getDay());
+      i++;
+    }
+  }
 
-	@Test public void testReadInput() {
-		final String testString = "March 12 2002";
-		provideIn(testString); 
-		
-		Date testInOutDate = new Date();
-		
-		testInOutDate.readInput();
-		assertEquals(3, testInOutDate.getMonth());
-		assertEquals(12, testInOutDate.getDay());
-		assertEquals(2002, testInOutDate.getYear());
+  @Test
+  public void testGetYear() {
+    Date myGetYear = new Date();
+    assertEquals(1000, myGetYear.getYear());
+  }
 
-		
-	 	}
-	
+  @Test
+  public void testToString() {
+    Date myToString = new Date();
+    assertEquals("January 1, 1000", myToString.toString());
+  }
+
+  @Test
+  public void testEqualsDate() {
+    Date testDate1 = new Date();
+    Date testDate2 = new Date();
+    assertTrue(testDate1.equals(testDate2));
+  }
+
+  @Test
+  public void testPrecedes() {
+    Date testDate1 = new Date();
+    Date testDate2 = new Date(2, 1, 1000);
+    Date testDate3 = new Date(1, 2, 1000);
+    Date testDate4 = new Date(1, 1, 2000);
+    assertTrue(testDate1.precedes(testDate2));
+    assertTrue(testDate1.precedes(testDate3));
+    assertTrue(testDate1.precedes(testDate4));
+
+  }
+
+  @Test
+  public void testReadInput() {
+    final String testString = "March 12 2002";
+    provideIn(testString);
+
+    Date testInOutDate = new Date();
+
+    testInOutDate.readInput();
+    assertEquals(3, testInOutDate.getMonth());
+    assertEquals(12, testInOutDate.getDay());
+    assertEquals(2002, testInOutDate.getYear());
+
+  }
+
 }
