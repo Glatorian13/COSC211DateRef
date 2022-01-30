4803f085b45ee06819652af4233b2628e719c5fb
Glatorian13 <glatorian13@gmail.com>
2022-01-30 13:51:02 -0500

    junit test fix

--- DateTest.java
+++ DateTest.java
@@ -1,20 +1,23 @@
 import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.*;
+
+import java.io.*;
 
 import org.junit.Test;
 
 import org.junit.*;
 
 public class DateTest {
-	
-	/* 
-	 * Cannot test methods that use System.exit because Eclipse is using JUnit 4.11. 
-	 * If on version 4.9 or newer, testing System.exit is simple
-	 * Java 17 deprecated the security manager that you can use to test
-	 * */
-	
+
+	/*
+	 * Cannot test methods that use System.exit because Eclipse is using JUnit 4.11.
+	 * If on version 4.9 or newer, testing System.exit is simple Java 17 deprecated
+	 * the security manager that you can use to test
+	 */
+
 	@Test
-	public void testDate( ) {
+	public void testDate() {
 		Date testDate = new Date();
 		assertEquals(1, testDate.getMonth());
 		assertEquals(1, testDate.getDay());
@@ -50,7 +53,7 @@ public class DateTest {
 	@Test
 	public void testSetYear() {
 		Date mySetYearInt = new Date();
-		for(int i = 1000; i < 9999; i++) {
+		for (int i = 1000; i < 9999; i++) {
 			mySetYearInt.setYear(i);
 			assertEquals(i, mySetYearInt.getYear());
 		}
@@ -60,34 +63,34 @@ public class DateTest {
 	public void testSetMonth() {
 		Date mySetMonthInt = new Date();
 		for (int i = 1; i < 13; i++) {
-		mySetMonthInt.setMonth(i);
-		assertEquals(i, mySetMonthInt.getMonth());
+			mySetMonthInt.setMonth(i);
+			assertEquals(i, mySetMonthInt.getMonth());
 		}
 		/*
 		 * mySetMonthInt.setMonth(0); assertEquals("Fatal Error",
 		 * mySetMonthInt.getMonth()); mySetMonthInt.setMonth(14);
 		 * assertEquals("Fatal Error", mySetMonthInt.getMonth());
 		 */
-		}
+	}
 
 	@Test
 	public void testSetDay() {
 		Date mySetDayInt = new Date();
 
 		for (int i = 1; i < 32; i++) {
-		mySetDayInt.setDay(i);
-		assertEquals(i, mySetDayInt.getDay());
+			mySetDayInt.setDay(i);
+			assertEquals(i, mySetDayInt.getDay());
 		}
 		/*
 		 * mySetDayInt.setDay(0); assertEquals("Fatal Error", mySetDayInt.getDay());
 		 * mySetDayInt.setDay(44); assertEquals("Fatal Error", mySetDayInt.getDay());
 		 */
-		}
+	}
 
-	@Test	
+	@Test
 	public void testGetMonth() {
 		Date myGetMonthInt = new Date();
-		
+
 		for (int i = 1; i < 13; i++) {
 			myGetMonthInt.setMonth(i);
 			assertEquals(i, myGetMonthInt.getMonth());
@@ -139,12 +142,47 @@ public class DateTest {
 
 	}
 
+	
+	//stuff testing user input
+	// https://stackoverflow.com/questions/1647907/junit-how-to-simulate-system-in-testing
+	private final InputStream sysIn = System.in;
+	private final PrintStream sysOut = System.out;
+	
+	private ByteArrayInputStream testIn;
+	private ByteArrayOutputStream testOut;
+	@Before
+	public void setUpOut() {
+		testOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(testOut));
+	}
+	
+	private void provideIn(String data) {
+		testIn = new ByteArrayInputStream(data.getBytes());
+		System.setIn(testIn);
+	}
+	
+	
 	/*
-	 * @Before
-	 * 
-	 * @After
-	 * 
-	 * @Test public void testReadInput() { fail("Not yet implemented"); // TODO } }
+	 * private String getOut() { return testOut.toString(); }
 	 */
+	@After
+	public void restoreOriginalInOut() {
+		System.setIn(sysIn);
+		System.setOut(sysOut);
+	}
+	
+	@Test public void testReadInput() {
+		final String testString = "March 12 2002";
+		provideIn(testString); 
+		
+		Date testInOutDate = new Date();
+		
+		testInOutDate.readInput();
+		assertEquals(3, testInOutDate.getMonth());
+		assertEquals(12, testInOutDate.getDay());
+		assertEquals(2002, testInOutDate.getYear());
 
+		
+	 	}
+	
 }
